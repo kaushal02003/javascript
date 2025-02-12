458097a5d9a0abb43b26fdc193c808dd984dabdd
kaushal02003 <kaushalpandey14022000@gmail.com>
2025-02-12 17:48:50 +0000

    changes

--- basics/comparison.js
+++ basics/comparison.js
@@ -9,9 +9,15 @@ console.log(2 <= 1); //if the 1st number is lesser than or equal to 2nd number  
 
 console.log("2" > 1);  
 console.log("02" > 1);
+console.log("2" == 2);
 
 console.log(null > 0);
-console.log(null == 0);// the answer here is false since equality operator doesn't convert different datatypes for comparison
+console.log(null == 0);// the answer here is false since equality operator doesn't convert null for comparison
 console.log(null >= 0);// the answer here is true since comparison operator convert diffrent dataype to same datatype
                        //  like here null is convereted to 0
+
+console.log(undefined >= 0); // undefined is converted to NaN
+console.log(undefined != 1); // undefined gives true when we compare it with loose inequality to any number
+console.log(undefined != 0);
+
 
